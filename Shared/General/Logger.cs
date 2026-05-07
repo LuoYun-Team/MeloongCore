9a77157dcf1d781f27fa92a054821b2678ff30aa
龙腾猫跃 <1043137532@qq.com>
2026-05-07 11:26:31 +0800

    ref: 迁移 ValueConverter

--- Shared/General/Logger.cs
+++ Shared/General/Logger.cs
@@ -56,7 +56,7 @@ public enum LogBehavior {
 }
 
 public static class Logger {
-    internal static BaseLogger Instance { get; set; } = new();
+    public static BaseLogger Instance { get; set; } = new();
 
     // 转发给实例的方法包装
     public static void Log(string message, LogLevel level = LogLevel.Info, LogBehavior behavior = LogBehavior.None, [CallerFilePath] string filePath = "") 
