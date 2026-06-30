1f91e757c27c0c4cda7137b96549123bff749631
龙腾猫跃 <1043137532@qq.com>
2026-07-01 07:17:49 +0800

    *

--- Shared/Utils/GeneralUtils.cs
+++ Shared/Utils/GeneralUtils.cs
@@ -1,10 +1,10 @@
-namespace MeloongCore.Extensions;
-public static class GeneralExtension {
+namespace MeloongCore;
+public static class GeneralUtils {
 
     /// <summary>
     /// 判断对象是否为指定泛型类型的实例。
     /// </summary>
-    public static bool IsGenericInstanceOf(this object? instance, Type genericTypeDefinition) {
+    public static bool IsGenericInstanceOf(object? instance, Type genericTypeDefinition) {
         if (instance is null) return false;
         for (var type = instance.GetType(); type is not null; type = type.BaseType) {
             if (type.IsGenericType && type.GetGenericTypeDefinition() == genericTypeDefinition) return true;
